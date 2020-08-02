1e2de7ceac18807c848a6ca9d6c40781b0fd192f
erik.wu36 <erik.wu@titansoft.com.sg>
2020-08-02 23:23:30 +0800

    add demo view

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,89 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
     <v-main>
-      <HelloWorld />
+      <v-row justify="center">
+        <v-col cols="6" style="textAlign:center">
+          <v-row>
+            <v-col cols="4">
+              <v-btn large color="error" width="100px" @click.prevent="prevPage" :disabled="pageButtonStatus">Prev Page</v-btn>
+            </v-col>
+            <v-col cols="4" style="textAlign:center">
+              <h1 page-size>{{ page }}</h1>
+            </v-col>
+            <v-col cols="4" style="textAlign:center">
+              <v-btn large color="primary" width="100px" @click.prevent="nextPage" :disabled="pageButtonStatus">Next Page</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" style="textAlign:center" v-if="userLists.length === 0">
+          <span>USER LIST IS EMPTY </span>
+          <br />
+          <span v-if="errorResp" style="color:red;fontSize:30px"> {{ errorMsg }} </span>
+        </v-col>
+        <v-col cols="4" v-else v-for="user of userLists" :key="user.mail">
+          <UserCard :user="user" user-info />
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import UserCard from '@/components/UserCard.vue'
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
-    HelloWorld
+    UserCard
   },
 
   data: () => ({
-    //
-  })
-};
+    page: 0,
+    userLists: [],
+    pageButtonStatus: false,
+    errorResp: false,
+    errorMsg: 'API CALL ERROR PLASE TRY AGAIN'
+  }),
+  watch: {
+    page: {
+      async handler() {
+        await this.getUsers()
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    async getUsers() {
+      this.pageButtonStatus = true
+      try {
+        const res = await this.$axios.get(`https://reqres.in/api/users/?page=${this.page}`)
+        const { data } = res.data
+        this.userLists = data
+        this.setError(false)
+      } catch {
+        this.setError(true)
+      } finally {
+        this.pageButtonStatus = true
+      }
+    },
+    setPageButtonStatus(status) {
+      this.pageButtonStatus = status
+    },
+    setError(status) {
+      if (status) {
+        this.userLists = []
+      }
+      this.errorResp = status
+    },
+    nextPage() {
+      this.page++
+    },
+    prevPage() {
+      this.page--
+    }
+  }
+}
 </script>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card class="" max-width="300" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">TITANSOFT</div>
+        <v-list-item-title class="headline mb-1">{{ fullName }}</v-list-item-title>
+        <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey">
+        <v-img :src="user.avatar" />
+      </v-list-item-avatar>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    fullName() {
+      const { first_name, last_name } = this.user
+      return `${first_name}_${last_name}`
+    }
+  }
+}
+</script>
